062c7627e32468909a2aa548e78de62686a1dcf4
Mrinal Anand <77539412+Mrinal540@users.noreply.github.com>
2023-05-20 12:27:17 +0000

    Day 1

--- first.java
+++ first.java
@@ -1,3 +1,5 @@
+//Write a program to print "Hey Mrinal Welcome to the world of computers !".
+//"Start from 0 whenever you fall"
 public class first{
 public static void main(String args[]){
     System.out.println("Hey Mrinal Welcome to the world of computers !");
